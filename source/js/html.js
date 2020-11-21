f7cfab9d8258f10e34c838f338c35f9f5d3b45ea
Abdinaasir15 <cabdinaasirc15@gmail.com>
2020-11-21 16:32:58 +0000

    is bedel cusub

--- source/js/html.js
+++ source/js/html.js
@@ -1,11 +1,15 @@
 // lists
 function hoyga() {
+    document.getElementById('now').style.display = 'none';
     document.getElementById('hoyga').style.display = 'block';
     document.getElementById('hordhaca').style.display = 'none';
     document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'none';
+    document.getElementById('tusmo').style.display = 'none';
+
     //     // colors
-    document.getElementById('item-1').style.backgroundColor = 'white';
-    document.getElementById('item-1').style.color = 'black';
+    // document.getElementById('item-1').style.backgroundColor = 'white';
+    // document.getElementById('item-1').style.color = 'black';
     // document.getElementById('item-2').style.backgroundColor = 'transparent';
     // document.getElementById('item-2').style.color = 'white';
     // document.getElementById('item-3').style.backgroundColor = 'transparent';
@@ -17,50 +21,66 @@ function hoyga() {
     // document.getElementById('item-6').style.backgroundColor = 'transparent';
     // document.getElementById('item-6').style.color = 'white';
 
-
     // end
-    // document.getElementById('now').style.display = 'none';
-    // document.getElementById('menu-2').style.width = '0px';
-    document.getElementById('main-content').style.marginLeft = '0px';
 }
 
 function hordhaca() {
+    document.getElementById('now').style.display = 'none';
     document.getElementById('hoyga').style.display = 'none';
     document.getElementById('hordhaca').style.display = 'block';
     document.getElementById('taariikh').style.display = 'none';
-
-    // colors
-    // document.getElementById('item-1').style.backgroundColor = 'transparent';
-    // document.getElementById('item-1').style.color = 'white';
-    // corr
-    document.getElementById('item-2').style.backgroundColor = 'white';
-    document.getElementById('item-2').style.color = 'black';
-    // end of corr
-    // document.getElementById('item-3').style.backgroundColor = 'transparent';
-    // document.getElementById('item-3').style.color = 'white';
-    // document.getElementById('item-4').style.backgroundColor = 'transparent';
-    // document.getElementById('item-4').style.color = 'white';
-    // document.getElementById('item-5').style.backgroundColor = 'transparent';
-    // document.getElementById('item-5').style.color = 'white';
-    // document.getElementById('item-6').style.backgroundColor = 'transparent';
-    // document.getElementById('item-6').style.color = 'white';
-
-    // document.getElementById('now').style.display = 'none';
-    // document.getElementById('menu-2').style.width = '0px';
-    document.getElementById('main-content').style.marginLeft = '0px';
+    document.getElementById('tifaftire').style.display = 'none';
+    document.getElementById('tusmo').style.display = 'none';
 
 }
 
 // taaariikh
 function taariikh() {
+    document.getElementById('now').style.display = 'none';
     document.getElementById('hoyga').style.display = 'none';
     document.getElementById('hordhaca').style.display = 'none';
     document.getElementById('taariikh').style.display = 'block';
+    document.getElementById('tifaftire').style.display = 'none';
+    document.getElementById('tusmo').style.display = 'none';
 
 
-    // document.getElementById('now').style.display = 'none';
-    // document.getElementById('menu-2').style.width = '0px';
-    document.getElementById('main-content').style.marginLeft = '0px';
+}
+
+// tifaftire
+function tifaftire() {
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'block';
+    document.getElementById('tusmo').style.display = 'none';
+
+
+
+}
+
+// wali
+function element() {
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'none';
+    document.getElementById('element').style.display = 'block';
+    document.getElementById('tusmo').style.display = 'none';
+
+}
+// end
+
+// tusmooyinka html ama html tags
+function tusmo() {
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'none';
+    document.getElementById('element').style.display = 'none';
+    document.getElementById('tusmo').style.display = 'block';
 
 }
 
@@ -72,22 +92,9 @@ function hoyga1() {
     document.getElementById('hoyga').style.display = 'block';
     document.getElementById('hordhaca').style.display = 'none';
     document.getElementById('taariikh').style.display = 'none';
-    //     // colors
-    document.getElementById('item-1').style.backgroundColor = 'white';
-    document.getElementById('item-1').style.color = 'black';
-    // document.getElementById('item-2').style.backgroundColor = 'transparent';
-    // document.getElementById('item-2').style.color = 'white';
-    // document.getElementById('item-3').style.backgroundColor = 'transparent';
-    // document.getElementById('item-3').style.color = 'white';
-    // document.getElementById('item-4').style.backgroundColor = 'transparent';
-    // document.getElementById('item-4').style.color = 'white';
-    // document.getElementById('item-5').style.backgroundColor = 'transparent';
-    // document.getElementById('item-5').style.color = 'white';
-    // document.getElementById('item-6').style.backgroundColor = 'transparent';
-    // document.getElementById('item-6').style.color = 'white';
+    document.getElementById('tifaftire').style.display = 'none';
 
 
-    // end
     document.getElementById('now').style.display = 'none';
     document.getElementById('menu-3').style.width = '0px';
     document.getElementById('main-content').style.marginLeft = '0px';
@@ -97,27 +104,12 @@ function hordhaca1() {
     document.getElementById('hoyga').style.display = 'none';
     document.getElementById('hordhaca').style.display = 'block';
     document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'none';
 
-    // colors
-    // document.getElementById('item-1').style.backgroundColor = 'transparent';
-    // document.getElementById('item-1').style.color = 'white';
-    // corr
-    document.getElementById('item-2').style.backgroundColor = 'white';
-    document.getElementById('item-2').style.color = 'black';
-    // end of corr
-    // document.getElementById('item-3').style.backgroundColor = 'transparent';
-    // document.getElementById('item-3').style.color = 'white';
-    // document.getElementById('item-4').style.backgroundColor = 'transparent';
-    // document.getElementById('item-4').style.color = 'white';
-    // document.getElementById('item-5').style.backgroundColor = 'transparent';
-    // document.getElementById('item-5').style.color = 'white';
-    // document.getElementById('item-6').style.backgroundColor = 'transparent';
-    // document.getElementById('item-6').style.color = 'white';
 
     document.getElementById('now').style.display = 'none';
     document.getElementById('menu-3').style.width = '0px';
     document.getElementById('main-content').style.marginLeft = '0px';
-
 }
 
 // taaariikh
@@ -125,10 +117,54 @@ function taariikh1() {
     document.getElementById('hoyga').style.display = 'none';
     document.getElementById('hordhaca').style.display = 'none';
     document.getElementById('taariikh').style.display = 'block';
+    document.getElementById('tifaftire').style.display = 'none';
 
 
     document.getElementById('now').style.display = 'none';
     document.getElementById('menu-3').style.width = '0px';
     document.getElementById('main-content').style.marginLeft = '0px';
+}
 
+// tifaftire
+function tifaftire1() {
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'block';
+
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('menu-3').style.width = '0px';
+    document.getElementById('main-content').style.marginLeft = '0px';
+}
+
+// wali
+
+function element1() {
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'none';
+    document.getElementById('element').style.display = 'block';
+    document.getElementById('tusmo').style.display = 'none';
+
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('menu-3').style.width = '0px';
+    document.getElementById('main-content').style.marginLeft = '0px';
+}
+// end
+
+// tusmooyinka html ama html tags
+function tusmo1() {
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'none';
+    document.getElementById('tifaftire').style.display = 'none';
+    document.getElementById('element').style.display = 'none';
+    document.getElementById('tusmo').style.display = 'block';
+
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('menu-3').style.width = '0px';
+    document.getElementById('main-content').style.marginLeft = '0px';
 }
