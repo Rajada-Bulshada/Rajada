85c81ebc9d07ac15de7d5b6436c79f0ec00f82be
Abdinaasir15 <cabdinaasirc15@gmail.com>
2020-11-21 11:19:06 +0000

    is  bedel dhanka aqrinta

--- source/js/html.js
+++ source/js/html.js
@@ -6,10 +6,10 @@ function hoyga() {
     //     // colors
     document.getElementById('item-1').style.backgroundColor = 'white';
     document.getElementById('item-1').style.color = 'black';
-    document.getElementById('item-2').style.backgroundColor = 'transparent';
-    document.getElementById('item-2').style.color = 'white';
-    document.getElementById('item-3').style.backgroundColor = 'transparent';
-    document.getElementById('item-3').style.color = 'white';
+    // document.getElementById('item-2').style.backgroundColor = 'transparent';
+    // document.getElementById('item-2').style.color = 'white';
+    // document.getElementById('item-3').style.backgroundColor = 'transparent';
+    // document.getElementById('item-3').style.color = 'white';
     // document.getElementById('item-4').style.backgroundColor = 'transparent';
     // document.getElementById('item-4').style.color = 'white';
     // document.getElementById('item-5').style.backgroundColor = 'transparent';
@@ -19,8 +19,8 @@ function hoyga() {
 
 
     // end
-    document.getElementById('now').style.display = 'none';
-    document.getElementById('menu-2').style.width = '0px';
+    // document.getElementById('now').style.display = 'none';
+    // document.getElementById('menu-2').style.width = '0px';
     document.getElementById('main-content').style.marginLeft = '0px';
 }
 
@@ -30,14 +30,14 @@ function hordhaca() {
     document.getElementById('taariikh').style.display = 'none';
 
     // colors
-    document.getElementById('item-1').style.backgroundColor = 'transparent';
-    document.getElementById('item-1').style.color = 'white';
+    // document.getElementById('item-1').style.backgroundColor = 'transparent';
+    // document.getElementById('item-1').style.color = 'white';
     // corr
     document.getElementById('item-2').style.backgroundColor = 'white';
     document.getElementById('item-2').style.color = 'black';
     // end of corr
-    document.getElementById('item-3').style.backgroundColor = 'transparent';
-    document.getElementById('item-3').style.color = 'white';
+    // document.getElementById('item-3').style.backgroundColor = 'transparent';
+    // document.getElementById('item-3').style.color = 'white';
     // document.getElementById('item-4').style.backgroundColor = 'transparent';
     // document.getElementById('item-4').style.color = 'white';
     // document.getElementById('item-5').style.backgroundColor = 'transparent';
@@ -45,8 +45,8 @@ function hordhaca() {
     // document.getElementById('item-6').style.backgroundColor = 'transparent';
     // document.getElementById('item-6').style.color = 'white';
 
-    document.getElementById('now').style.display = 'none';
-    document.getElementById('menu-2').style.width = '0px';
+    // document.getElementById('now').style.display = 'none';
+    // document.getElementById('menu-2').style.width = '0px';
     document.getElementById('main-content').style.marginLeft = '0px';
 
 }
@@ -57,15 +57,56 @@ function taariikh() {
     document.getElementById('hordhaca').style.display = 'none';
     document.getElementById('taariikh').style.display = 'block';
 
-    // colors
-    document.getElementById('item-1').style.backgroundColor = 'transparent';
-    document.getElementById('item-1').style.color = 'white';
-    document.getElementById('item-2').style.backgroundColor = 'transparent';
-    document.getElementById('item-2').style.color = 'white';
-    // corr
-    document.getElementById('item-3').style.backgroundColor = 'white';
-    document.getElementById('item-3').style.color = 'black';
+
+    // document.getElementById('now').style.display = 'none';
+    // document.getElementById('menu-2').style.width = '0px';
+    document.getElementById('main-content').style.marginLeft = '0px';
+
+}
+
+
+
+// for phines
+
+function hoyga1() {
+    document.getElementById('hoyga').style.display = 'block';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'none';
+    //     // colors
+    document.getElementById('item-1').style.backgroundColor = 'white';
+    document.getElementById('item-1').style.color = 'black';
+    // document.getElementById('item-2').style.backgroundColor = 'transparent';
+    // document.getElementById('item-2').style.color = 'white';
+    // document.getElementById('item-3').style.backgroundColor = 'transparent';
+    // document.getElementById('item-3').style.color = 'white';
+    // document.getElementById('item-4').style.backgroundColor = 'transparent';
+    // document.getElementById('item-4').style.color = 'white';
+    // document.getElementById('item-5').style.backgroundColor = 'transparent';
+    // document.getElementById('item-5').style.color = 'white';
+    // document.getElementById('item-6').style.backgroundColor = 'transparent';
+    // document.getElementById('item-6').style.color = 'white';
+
+
     // end
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('menu-3').style.width = '0px';
+    document.getElementById('main-content').style.marginLeft = '0px';
+}
+
+function hordhaca1() {
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'block';
+    document.getElementById('taariikh').style.display = 'none';
+
+    // colors
+    // document.getElementById('item-1').style.backgroundColor = 'transparent';
+    // document.getElementById('item-1').style.color = 'white';
+    // corr
+    document.getElementById('item-2').style.backgroundColor = 'white';
+    document.getElementById('item-2').style.color = 'black';
+    // end of corr
+    // document.getElementById('item-3').style.backgroundColor = 'transparent';
+    // document.getElementById('item-3').style.color = 'white';
     // document.getElementById('item-4').style.backgroundColor = 'transparent';
     // document.getElementById('item-4').style.color = 'white';
     // document.getElementById('item-5').style.backgroundColor = 'transparent';
@@ -74,8 +115,20 @@ function taariikh() {
     // document.getElementById('item-6').style.color = 'white';
 
     document.getElementById('now').style.display = 'none';
-    document.getElementById('menu-2').style.width = '0px';
+    document.getElementById('menu-3').style.width = '0px';
     document.getElementById('main-content').style.marginLeft = '0px';
 
 }
 
+// taaariikh
+function taariikh1() {
+    document.getElementById('hoyga').style.display = 'none';
+    document.getElementById('hordhaca').style.display = 'none';
+    document.getElementById('taariikh').style.display = 'block';
+
+
+    document.getElementById('now').style.display = 'none';
+    document.getElementById('menu-3').style.width = '0px';
+    document.getElementById('main-content').style.marginLeft = '0px';
+
+}
--- source/js/index.js
+++ source/js/index.js
@@ -1,40 +1,32 @@
 function openmenu() {
-document.getElementById('menu').style.width = '200px';
-document.getElementById('main').style.marginRight = '200px';
+    document.getElementById('menu').style.width = '200px';
+    document.getElementById('main').style.marginRight = '200px';
 }
 
 function closemenu() {
-document.getElementById('menu').style.width = '0px';
-document.getElementById('main').style.marginRight = '0px';
+    document.getElementById('menu').style.width = '0px';
+    document.getElementById('main').style.marginRight = '0px';
 }
 // phone
 function phonemenu() {
     document.getElementById('menu-phone').style.width = '100%';
     document.getElementById('main').style.marginRight = '10px';
-    }
-    
-    function clsmenu() {
+}
+
+function clsmenu() {
     document.getElementById('menu-phone').style.width = '0px';
     document.getElementById('main').style.marginRight = '0px';
-    }
-    
+}
 
+
+// for web design page
 // for html page
 function furmenu() {
-    document.getElementById('menu-2').style.width = '260px';
+    document.getElementById('menu-3').style.width = '260px';
     document.getElementById('main-content').style.marginLeft = '260px';
-    }
-    function xirmenu() {
-    document.getElementById('menu-2').style.width = '0px';
-    document.getElementById('main-content').style.marginLeft = '0px';
-    }
+}
 
-    // for css page
-// function furmenu() {
-//     document.getElementById('menu-2').style.width = '120px';
-//     document.getElementById('main-content').style.marginLeft = '120px';
-//     }
-//     function xirmenu() {
-//     document.getElementById('menu-2').style.width = '0px';
-//     document.getElementById('main-content').style.marginLeft = '0px';
-//     }
+function xirmenu() {
+    document.getElementById('menu-3').style.width = '0px';
+    document.getElementById('main-content').style.marginLeft = '0px';
+}
